7025318b80ae5b0c253afd52047b94c74d2e75f0
Atos016 <atosgrego82@gmail.com>
2025-11-13 22:12:08 -0300

    Create SmartBaritoneMod class for mod initialization

---
+++ smart_baritone_mod/scr/main/java/com/SmartBaritoneMod.java
@@ -0,0 +1,11 @@
+
+package com.smartbaritone;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SmartBaritoneMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Smart Baritone carregado!");
+    }
+}
